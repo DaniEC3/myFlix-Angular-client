106f3f10468f6536517732de83ca189a940f0da1
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2025-06-23 10:26:38 -0400

    Header html and style added

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {  RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.html',
   styleUrls: ['./header.scss']
 })
 export class HeaderComponent {
+menuOpen = false;
 
+ toggleMenu() {
+  this.menuOpen = !this.menuOpen;
+ }
 }
