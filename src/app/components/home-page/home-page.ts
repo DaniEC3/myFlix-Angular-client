5f1ee7ecae5819299995f31849064708e949415e
DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
2025-07-08 20:19:36 -0400

    Squashed commit of the following:
    commit 012698fada84b96c3e81aabc656c536b96abb079
    Author: DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
    Date:   Tue Jul 8 20:19:10 2025 -0400
    
        Movie-card styles added, show/hide button in login form styled, general styling bugs fixed
    
    commit e4a4bff8281556524e4a87505a4fe194aa24d054
    Author: DaniEC3 <167336437+DaniEC3@users.noreply.github.com>
    Date:   Thu Jul 3 23:46:57 2025 -0400
    
        movie card added

--- src/app/components/home-page/home-page.ts
+++ src/app/components/home-page/home-page.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
 import { RouterModule, Router } from '@angular/router';
+import { MovieCardComponent } from '../movie-card/movie-card';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
-    MatCardModule,
-    MatButtonModule,
+    MovieCardComponent,
     RouterModule,
   ],
   templateUrl: './home-page.html',
